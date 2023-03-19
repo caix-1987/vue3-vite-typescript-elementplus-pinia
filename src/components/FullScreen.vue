bdc03fd3eb0ea1e3babdc219211e58240dccd586
caix-1987 <632816861@qq.com>
2023-03-19 12:03:24 +0800

    1、安装全屏功能插件 screenfull 2、全屏功能通用组件逻辑开发

--- src/components/FullScreen.vue
+++ src/components/FullScreen.vue
@@ -1,9 +1,55 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onUnmounted } from "vue";
+import { ElMessage } from "element-plus";
+import screenfull from "screenfull";
+
+const props = defineProps({
+  element: {
+    type: String,
+    default: "html",
+  },
+  openTips: {
+    type: String,
+    default: "全屏",
+  },
+  closeTips: {
+    type: String,
+    default: "退出全屏",
+  },
+});
+
+const tips = ref<string>(props.openTips);
+const isFullscreen = ref<boolean>(false);
+
+const click = () => {
+  const dom = document.querySelector(props.element) || undefined;
+  if (!screenfull.isEnabled) {
+    ElMessage.warning("您的浏览器不支持全屏");
+    return;
+  }
+  screenfull.toggle(dom);
+};
+
+const change = () => {
+  isFullscreen.value = screenfull.isFullscreen;
+  tips.value = screenfull.isFullscreen ? props.closeTips : props.openTips;
+};
+
+screenfull.on("change", change);
+
+onUnmounted(() => {
+  if (screenfull.isEnabled) {
+    screenfull.off("change", change);
+  }
+});
+</script>
 
 <template>
-  <div class="app-fullScreen">
-    <el-tooltip effect="dark" content="全屏" placement="bottom">
-      <el-icon class="icon"><FullScreen /></el-icon>
+  <div class="app-fullScreen" @click="click">
+    <el-tooltip effect="dark" :content="tips" placement="bottom">
+      <el-icon class="icon">
+        <full-screen />
+      </el-icon>
     </el-tooltip>
   </div>
 </template>
