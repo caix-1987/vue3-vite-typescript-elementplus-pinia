8a9eb5bada9f74c251edfb90631ade0bd9dfb30c
caix-1987 <632816861@qq.com>
2023-03-19 12:30:01 +0800

    个人信息组件功能开发

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,28 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="app-userInfo">
-    <img src="@/assets/caix.png" alt="" />
-    <span>caix</span>
-  </div>
+  <el-dropdown size="large" placement="bottom-start">
+    <div class="app-userInfo">
+      <img src="@/assets/caix.png" alt="" />
+      <span>caix</span>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <a
+          target="_blank"
+          href="https://github.com/caix-1987/vue3-vite-typescript-elementplus-pinia"
+        >
+          <el-dropdown-item> gitHub 地址 </el-dropdown-item>
+        </a>
+        <a href="javascript:void">
+          <el-dropdown-item> 进入个人博客 </el-dropdown-item>
+        </a>
+        <el-dropdown-item divided>
+          <span>退出登录</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
 
 <style lang="scss" scoped>
@@ -18,6 +36,9 @@
     height: 30px;
     border-radius: 30px;
     margin-right: 10px;
+  }
+  span {
+    color: #ffffff;
   }
 }
 </style>
