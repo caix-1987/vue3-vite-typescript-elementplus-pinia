89911e1f8a0dab8b55e8f56c2598a7a2a69c3476
caix-1987 <632816861@qq.com>
2023-03-28 18:00:08 +0800

    打包dist

---
+++ dist/assets/403-7f77e4ed.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,c as t}from"./index-baf6e919.js";const n={},o={class:"app-content"};function s(a,_){return c(),t("div",o,"page 403")}const p=e(n,[["render",s]]);export{p as default};
---
+++ dist/assets/caix-2f52b59e.js
@@ -0,0 +1,1 @@
+const s="/assets/caix-08ec6790.png";export{s as _};
